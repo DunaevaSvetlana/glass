3173db3ee25877cac5597af74e0d85776b10caf2
DunaevaSvetlana <solnste1993@mail.ru>
2020-03-12 00:43:07 +0400

    Initial commit

---
+++ src/glass.java
@@ -0,0 +1,13 @@
+public class glass {
+    public static void main(String[] args) {
+        final int volumeMl = 200;
+        final double full = 0.5;
+
+        final double volumeGlass = volumeMl * full;
+
+        System.out.println("volumeMl = " + volumeMl);
+        System.out.println("full = " + full);
+        
+        System.out.println("volumeGlass = " + volumeGlass);
+    }
+}
